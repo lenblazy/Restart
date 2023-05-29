c1d292a79a37ac1f32fdafd6836e9cd411c59480
lenblazy <lenblazy@gmail.com>
2023-05-29 16:00:08 +0300

    Add play sound functionality

--- Restart/Screens/OnboardingView.swift
+++ Restart/Screens/OnboardingView.swift
@@ -159,6 +159,7 @@ struct OnboardingView: View {
                                 .onEnded{ _ in
                                     withAnimation(Animation.easeOut(duration: 0.5)) {
                                         if buttonOffset > buttonWidth / 2 {
+                                            playSound(sound: "chimeup", type: "mp3")
                                             buttonOffset = buttonWidth - 80
                                             isOnboardingActive = false
                                         } else {
@@ -188,7 +189,7 @@ struct OnboardingView: View {
         .onAppear {
             isAnimating = true
         }
-        
+        .preferredColorScheme(.dark)
         
     }
 }
